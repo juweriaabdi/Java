7ee5ef1388953e39c3a19535413c05cd33d34216
juweriaabdi <tijaaboyar2022@gmail.com>
2025-07-10 21:57:32 +0300

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[10];
+
+        System.out.println("Enter 10 integers:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            numbers[i] = input.nextInt();
+        }
+
+        System.out.println("\nNumbers in reverse order:");
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
